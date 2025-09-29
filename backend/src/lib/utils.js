87472a236288239739f593aa6d408efb4514a8b8
alishase <maksimzorin10@gmail.com>
2025-09-29 22:11:55 +0200

    signup section code refactoring

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
+  if (!process.env.JWT_SECRET) throw new Error("JWT_SECRET is not defined");
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
