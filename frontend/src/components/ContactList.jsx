f77113149ea7de3f629c01b9509219f9fff727f8
alishase <maksimzorin10@gmail.com>
2025-10-04 14:09:19 +0200

    sidebar completed (change express json limit to 10mb on backend)

--- frontend/src/components/ContactList.jsx
+++ frontend/src/components/ContactList.jsx
@@ -1,3 +1,40 @@
+import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+import UsersLoadingSkeleton from "./UsersLoadingSkeleton.jsx";
+
 export default function ContactList() {
-  return <div>ContactList</div>;
+  const { getAllContacts, allContacts, setSelectedUser, isUsersLoading } =
+    useChatStore();
+
+  useEffect(() => {
+    getAllContacts();
+  }, [getAllContacts]);
+
+  if (isUsersLoading) return <UsersLoadingSkeleton />;
+
+  return (
+    <>
+      {allContacts.map((contact) => (
+        <div
+          key={contact._id}
+          onClick={() => setSelectedUser(contact)}
+          className="bg-cyan-500/10 p-4 rounded-lg cursor-pointer hover:bg-cyan-500/20 transition-colors"
+        >
+          <div className="flex items-center gap-3">
+            <div className={`avatar online`}>
+              <div className="size-12 rounded-full">
+                <img
+                  src={contact.profilePic || "/avatar.png"}
+                  alt={contact.fullName}
+                />
+              </div>
+            </div>
+            <h4 className="text-slate-200 font-medium truncate">
+              {contact.fullName}
+            </h4>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 }
